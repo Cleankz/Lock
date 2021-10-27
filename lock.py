d54317d8c256576b71e30053c685462fc46851bb
Cleankz <92687535+Cleankz@users.noreply.github.com>
2021-10-27 22:07:46 +0300

    Create lock.py

---
+++ lock.py
@@ -0,0 +1,25 @@
+
+def PatternUnlock(N,hits = []):
+    case = ['15','51','26','62','18','81','29','92','27','72','38','83','24','42','53','35']
+    list_to_string = " ".join(str(x) for x in hits)
+    string_split = list_to_string.split()
+    hits_string = ''.join(string_split)
+    lenght = 0
+    for i in range(len(case)):
+        if case[i] in hits_string:
+            lenght = lenght + 1.40515
+    for i in range(N-1):
+        if hits[i+1] - hits[i] == 1:
+            lenght = lenght + 1
+    int_to_str = ''.join(str(lenght))
+    res_str_1 = int_to_str.replace('.', '') 
+    res_str_2 = res_str_1.replace('0', '')
+    list_lenght = list(res_str_2)
+    new_list_lenght = []
+
+    for i in range(len( list_lenght)):
+        new_list_lenght.append( list_lenght[i])
+            
+    int_to_str = ''.join(new_list_lenght)
+    
+    return int_to_str
