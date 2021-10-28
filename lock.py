2ef80097e990b59bc6625b826d1531dc440dc709
Cleankz <92687535+Cleankz@users.noreply.github.com>
2021-10-28 18:46:03 +0300

    Update lock.py

--- lock.py
+++ lock.py
@@ -7,31 +7,25 @@ def PatternUnlock(N,hits = []):
     string_split = list_to_string.split()
     hits_string = ''.join(string_split)
     lenght = 0
-    for i in range(len(case)):
-        if case[i] in hits_string:
+    for case1 in range(len(case)):
+        if case[case1] in hits_string:
             lenght = lenght + 1.40515
-    for l in range(len(case_2)):
-        if case_2[l] in hits_string:
+    for case2 in range(len(case_2)):
+        if case_2[case2] in hits_string:
             lenght = lenght + 1
-    
-    
-    for p in range(len(case_3)):
-        if case_2[p] in hits_string:
+    for case in range(len(case_3)):
+        if case_2[case ] in hits_string:
             lenght = lenght + 1
-    
-    for i in range(N-1):
-        if hits[i+1] - hits[i] <= 1 and hits[i+1] - hits[i] >= 0 :
+    for ind in range(N-1):
+        if hits[ind+1] - hits[ind] <= 1 and hits[ind+1] - hits[ind] >= 0 :
             lenght = lenght + 1
     int_to_str = ''.join(str(lenght))
     res_str_1 = int_to_str.replace('.', '') 
     res_str_2 = res_str_1.replace('0', '')
     list_lenght = list(res_str_2)
     new_list_lenght = []
-
     for i in range(len( list_lenght)):
         new_list_lenght.append( list_lenght[i])
-            
     int_to_str = ''.join(new_list_lenght)
-    
     return int_to_str
-
+print(PatternUnlock(3, [2,1,9]))
