1b18e5aa63a14537d71362ca4910b79eb376e9de
Cleankz <92687535+Cleankz@users.noreply.github.com>
2021-10-28 18:07:29 +0300

    Update lock.py

--- lock.py
+++ lock.py
@@ -2,6 +2,7 @@
 def PatternUnlock(N,hits = []):
     case = ['15','51','26','62','18','81','29','92','27','72','38','83','24','42','53','35']
     case_2 = ['91','21','61','82','52','32','65','54','98','87','43','73']
+    case_3 = ['19','16','28','25','37']
     list_to_string = " ".join(str(x) for x in hits)
     string_split = list_to_string.split()
     hits_string = ''.join(string_split)
@@ -13,8 +14,13 @@ def PatternUnlock(N,hits = []):
         if case_2[l] in hits_string:
             lenght = lenght + 1
     
+    
+    for p in range(len(case_3)):
+        if case_2[p] in hits_string:
+            lenght = lenght + 1
+    
     for i in range(N-1):
-        if hits[i+1] - hits[i] == 1:
+        if hits[i+1] - hits[i] <= 1 and hits[i+1] - hits[i] >= 0 :
             lenght = lenght + 1
     int_to_str = ''.join(str(lenght))
     res_str_1 = int_to_str.replace('.', '') 
@@ -28,3 +34,4 @@ def PatternUnlock(N,hits = []):
     int_to_str = ''.join(new_list_lenght)
     
     return int_to_str
+
